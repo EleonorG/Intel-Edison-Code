6465d275c5434d955428e50c2cd97c16bbd47a02
transistorintransit <ehead94@gmail.com>
2015-07-24 18:09:06 -0700

    Image file has been updated

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -3,13 +3,13 @@ import cv2
 import urllib
 
 # Grab the image
-urllib.urlretrieve("http://download.intel.com/newsroom/bios/images/web/brian-krzanich_1.jpg", "BK.jpg")
+urllib.urlretrieve("http://stephaniemoyerman.com/wp-content/uploads/2015/06/DSC_0713_linkedin.jpg", "Steph.jpg")
 
 # And grab the config file for facial recognition
 urllib.urlretrieve("https://raw.githubusercontent.com/Itseez/opencv/master/data/haarcascades/haarcascade_frontalface_alt.xml","haarcascade_frontalface_alt.xml")
 
 # Use openCV to load the image
-img = cv2.imread("BK.jpg")
+img = cv2.imread("Steph.jpg")
 
 # And convert to gray-scale for processing
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
